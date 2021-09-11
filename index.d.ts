528eb953f8de00c1265aa0b69c9e03af641d74c0
Duy Đat Phạm <duydatpham@gmail.com>
2021-09-11 14:49:03 +0700

    #update

--- index.d.ts
+++ index.d.ts
@@ -2,17 +2,17 @@ import * as React from 'react';
 import { ViewStyle } from 'react-native';
 
 export interface BlurViewProperties {
-  blurType: 'xlight' | 'light' | 'dark'
-    // tvOS only
-    | 'extraDark' | 'regular' | 'prominent';
+  blurType?: 'xlight' | 'light' | 'dark'
+  // tvOS only
+  | 'extraDark' | 'regular' | 'prominent';
   blurAmount?: number; // 0 - 100
   style?: ViewStyle;
   viewRef?: number | null;
 }
 
-export class BlurView extends React.Component<BlurViewProperties, {}> {}
+export class BlurView extends React.Component<BlurViewProperties, {}> { }
 
 
-export interface VibrancyViewProperties extends BlurViewProperties {}
+export interface VibrancyViewProperties extends BlurViewProperties { }
 
-export class VibrancyView extends React.Component<VibrancyViewProperties, {}> {}
+export class VibrancyView extends React.Component<VibrancyViewProperties, {}> { }
